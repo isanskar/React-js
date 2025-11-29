05ebcb41496414f2aedf8a56f71546e4497c4648
sanskar <sanskarshrivastava31103@gmail.com>
2025-11-29 16:38:35 +0530

    parcel

---
+++ 06_PARCEL/script.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client"
+
+const h1 = <h1> Hello World !!!!!</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(h1)
+
+console.log('hello world');
